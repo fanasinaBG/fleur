bdb63702fa2182a152c18eace53c5a13247dc39a
fanasinaBG <167691077+fanasinaBG@users.noreply.github.com>
2025-01-14 19:01:36 +0300

    base

--- WEB-INF/classes/DatabaseConnection.java
+++ WEB-INF/classes/DatabaseConnection.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
+public class DatabaseConnection{
     public static Connection getConnection() throws SQLException {
         Connection connection = null;
         try {
